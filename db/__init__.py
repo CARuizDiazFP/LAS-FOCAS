f1134f76e8c7ae7880d975b54dcbad5c58a7f934
FOCAL-POINT <focal@debian>
2026-01-07 14:23:45 -0300

    [db] Agregar __init__.py para corregir resolución de imports

---
+++ db/__init__.py
@@ -0,0 +1,3 @@
+# Nombre de archivo: __init__.py
+# Ubicación de archivo: db/__init__.py
+# Descripción: Inicialización del paquete db
---
+++ db/models/__init__.py
@@ -0,0 +1,3 @@
+# Nombre de archivo: __init__.py
+# Ubicación de archivo: db/models/__init__.py
+# Descripción: Inicialización del paquete db.models
